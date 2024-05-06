c0d79b38df23124f87e6429771ae11fb49b11e44
caramayo <caramayo@bancocredicoop.coop>
2024-05-06 18:14:37 -0300

    first commit

---
+++ validation.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById("contactForm");
+    form.addEventListener("submit", function(event) {
+        var nombre = document.getElementById("nombre").value;
+        var email = document.getElementById("email").value;
+        var mensaje = document.getElementById("mensaje").value;
+        var motivo = document.getElementById("motivo").value;
+        
+        if (nombre === "" || email === "" || mensaje === "" || motivo === "") {
+            alert("Por favor completa todos los campos obligatorios.");
+            event.preventDefault();
+        }
+    });
+});
